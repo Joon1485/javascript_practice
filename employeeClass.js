1afeb2d629536065a1bbf418b27d8b278423755b
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2022-05-28 15:34:13 +0900

    Update employeeClass.js

--- employeeClass.js
+++ employeeClass.js
@@ -12,3 +12,18 @@ class Employee {
 
 class PartTimeWorker extends Employee {
     static PAY_LATE = 8000;
+    constructor(name, depName, workingTime) {
+        super(name, depName, workingTime, PartTimeWorker.PAY_LATE) 
+    }
+    payCalc() {
+        return this.workingTime * PartTimeWorker.PAY_LATE;
+    }
+}
+
+const employee1 = new Employee('Jun', 'IT', '50');
+console.log(employee1);
+console.log(employee1.payCalc());
+
+const partEmployee1 = new PartTimeWorker('David', 'IT', '30');
+console.log(partEmployee1);
+console.log(partEmployee1.payCalc());
