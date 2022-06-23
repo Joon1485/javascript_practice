6fbc370ae6eb33a38846792b976affd5ed9cfe8c
jun1485 <65483521+jun1485@users.noreply.github.com>
2022-06-23 11:06:12 +0900

    Create template_literal.js

---
+++ template_literal.js
@@ -0,0 +1,11 @@
+
+    console.log("'I'm Fine Thank You !");
+    console.log('"시작이 반이다 !"');
+    console.log('"시작이 반"인데 ' + '왜 아직 시작을 안해!!');
+    console.log(`이왕 시작한거, '끝'을 보자 !!`); //Template Literal
+
+    let name = '정주니';
+    let word = '최고';
+
+    let string = `${name}는 ${word}다!!`;
+    console.log(string);
