9a12b40157e4f7724eb1256b2089567088a30ce3
주정준 <65483521+jun1485@users.noreply.github.com>
2022-07-02 10:06:03 +0900

    Create pos-sticky.js

---
+++ pos-sticky.js
@@ -0,0 +1,15 @@
+<div class="grey" style="background: lightgrey; height: 2000px; margin-top: 500px;">
+        <div class="image" style="float: right; width:700px; height: 200px; position: sticky; top:100px;">
+            <img style="width:100%;" src="https://www.apple.com/v/apple-tv-4k/y/images/overview/rooms/screen_arcade__ettndgk6b7ma_large.jpg" alt="">
+        </div>
+        <div class="text" style="float: left; width: 400px; margin-bottom: 500px;clear:both;">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit obcaecati quibusdam voluptates? Quisquam impedit eligendi corporis corrupti! Reiciendis at corrupti dicta ipsum. Rerum repellat, rem quo magni nostrum aperiam eum.
+        </div>
+        <div class="text" style="float: left; width: 400px; margin-bottom: 500px;clear:both;">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit obcaecati quibusdam voluptates? Quisquam impedit eligendi corporis corrupti! Reiciendis at corrupti dicta ipsum. Rerum repellat, rem quo magni nostrum aperiam eum.
+        </div>
+        <div class="text" style="float: left; width: 400px; margin-bottom: 500px;clear:both;">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit obcaecati quibusdam voluptates? Quisquam impedit eligendi corporis corrupti! Reiciendis at corrupti dicta ipsum. Rerum repellat, rem quo magni nostrum aperiam eum.
+        </div>
+        
+    </div>  
