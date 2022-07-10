6ced7c5780b596c0f5d7e2322ce4ac47a4ccb8ec
주정준 <65483521+jun1485@users.noreply.github.com>
2022-07-10 10:06:20 +0900

    Update clothShoppingExample.js

--- clothShoppingExample.js
+++ clothShoppingExample.js
@@ -47,3 +47,27 @@
         document.querySelector('.select-item').addEventListener('change', function () {
             document.querySelector('.select-size').classList.add('show-box');
         })
+
+        let products = [{
+                id: 0,
+                price: 80000,
+                title: 'Blossom Dress'
+            },
+            {
+                id: 1,
+                price: 50000,
+                title: 'Springfield Shirt'
+            },
+            {
+                id: 2,
+                price: 60000,
+                title: 'Black Monastery'
+            }
+        ];
+        let nameIndex = document.querySelectorAll('.product_name');
+        let priceIndex = document.querySelectorAll('.product_price');
+
+        document.querySelector('.filterBtn').addEventListener('click', function () {
+            let newProducts = products.filter(function (a) {
+                return a.price <= 60000;
+            });
