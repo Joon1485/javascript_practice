facd66a0c50a6da4bd5ba5baf0e00b83bdd8294e
주정준 <65483521+jun1485@users.noreply.github.com>
2022-07-26 19:31:51 +0900

    Update clothShoppingExample.js

--- clothShoppingExample.js
+++ clothShoppingExample.js
@@ -152,7 +152,23 @@
 
         let buyingProduct = document.querySelectorAll('.buy-btn');
         for (let i = 0; i < buyingProduct.length; i++) {
-            buyingProduct[i].addEventListener('click', function () { });
+            buyingProduct[i].addEventListener('click', function () {
+                if (localStorage.getItem('productName') != null) {
+                    let popedProductName = JSON.parse(localStorage.productName);
+                    popedProductName.push(products[i].title);
+                    localStorage.setItem('productName', JSON.stringify(popedProductName));
+                } else {
+                    let strToArr = products[i].title;
+                    localStorage.setItem('productName', JSON.stringify([strToArr]));
+                }
+            });
+        }
+    </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
