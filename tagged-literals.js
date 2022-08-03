e921548c2336366a484a2b759430b3c00f048484
주정준 <65483521+jun1485@users.noreply.github.com>
2022-08-03 20:11:05 +0900

    Create tagged-literals.js

---
+++ tagged-literals.js
@@ -0,0 +1,12 @@
+
+    <script>
+        var pants = 0;
+        var socks = 100;
+
+
+        function deconstruction(words, variable1, variable2) {
+            if (variable1 === 0) {
+                console.log(`바지 안팔아요 ${words[1]} ${variable2}`);
+                return;
+            }
+    </script>
